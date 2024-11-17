c07d23350dd613899ee09fb3d5cee2022dec7224
agentN86 <agentn86@duck.com>
2024-11-16 18:36:10 -0600

    updated changelog

--- script.js
+++ script.js
@@ -3,7 +3,44 @@ Array.from(document.getElementsByClassName('circle')).forEach(element => {
         document.getElementById('clicksound').play()
     });
 });
+var intro = false
 
+function changelog() {
+    document.getElementById('changelog').showModal()
+}
+
+//intro
+document.getElementById('intro').addEventListener("mouseenter", function() {
+    if (intro) {return}
+    intro = true
+    document.getElementById('hoversound').currentTime = 0
+    document.getElementById('hoversound').play()
+    var circle = document.getElementById('intro')
+    var agentsgams = document.getElementById('agentsgams')
+    var agentsunblckr = document.getElementById('agentsunblckr')
+    var agentsemulator = document.getElementById('agentsemulator')
+    var agentsyaproom = document.getElementById('agentsyaproom')
+
+    circle.classList.add("spin")
+    document.body.style.backgroundColor="black"
+
+    setTimeout(function() {
+        agentsgams.style.opacity = 1
+        agentsgams.classList.remove("hidden")
+        setTimeout(function() {
+            agentsunblckr.style.opacity = 1
+            agentsunblckr.classList.remove('hidden')
+            setTimeout(function() {
+                agentsemulator.style.opacity =1
+                agentsemulator.classList.remove('hidden')
+                setTimeout(function() {
+                    agentsyaproom.style.opacity =1
+                    agentsyaproom.classList.remove('hidden')
+                }, 200)
+            }, 200)
+        }, 200)
+    },600)
+})
 
 //gams
 document.getElementById('agentsgams').addEventListener('mouseenter', function() {
@@ -29,6 +66,19 @@ document.getElementById('agentsyaproom').addEventListener('mouseenter', function
 document.getElementById('agentsyaproom').addEventListener('mouseleave', function() {
     document.getElementById('img_yaproom').classList.add('hidden')
     document.getElementById('text_yaproom').classList.remove('hidden')
+})
+
+//emulator
+document.getElementById('agentsemulator').addEventListener('mouseenter', function() {
+    document.getElementById('hoversound').currentTime = 0
+    document.getElementById('hoversound').play()
+    document.getElementById('img_emulator').classList.remove('hidden')
+    document.getElementById('text_emulator').classList.add('hidden')
+})
+
+document.getElementById('agentsemulator').addEventListener('mouseleave', function() {
+    document.getElementById('img_emulator').classList.add('hidden')
+    document.getElementById('text_emulator').classList.remove('hidden')
 })
 
 //nowgg
